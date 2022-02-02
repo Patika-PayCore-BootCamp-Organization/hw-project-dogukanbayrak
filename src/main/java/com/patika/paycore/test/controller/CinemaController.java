e529d693857d73360e0e1ffff68c8e6e38bc524c
dogukanbayrak6767@gmail.com <dogukanbayrak6767@gmail.com>
2022-02-02 17:53:09 +0300

    CinemaController defined

--- src/main/java/com/patika/paycore/test/controller/CinemaController.java
+++ src/main/java/com/patika/paycore/test/controller/CinemaController.java
@@ -1,6 +1,8 @@
 package com.patika.paycore.test.controller;
 
 import com.patika.paycore.test.model.Cinema;
+import com.patika.paycore.test.service.CinemaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,26 +15,24 @@ import java.util.List;
 @RequestMapping("/cinema")
 public class CinemaController {
 
-    private List<Cinema> cinemas = new ArrayList<>();
-    {
-        cinemas.add(new Cinema("Cinemaximum"));
-        cinemas.add(new Cinema("Cinemapink"));
-    }
 
+    @Autowired
+    private CinemaService cinemaService;
 
-    @RequestMapping(method = RequestMethod.GET,path ="hello" )
-    public String hello(){
+    @RequestMapping(method = RequestMethod.GET, path = "hello")
+    public String hello() {
         return " Hello Patika";
     }
 
-    @RequestMapping(method = RequestMethod.GET,path ="/all" )
-    public List<Cinema> getAllCinemas(){
-        return cinemas;
+    @RequestMapping(method = RequestMethod.GET, path = "/all")
+    public List<Cinema> getAllCinemas() {
+        return cinemaService.getAllCinemas();
+
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/add")
-    public boolean addCinema(@RequestBody  Cinema cinema) {
-        return cinemas.add(cinema);
+    public boolean addCinema(@RequestBody Cinema cinema) {
+        return cinemaService.addCinema(cinema);
 
     }
 
