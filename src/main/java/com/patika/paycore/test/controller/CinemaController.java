0c3ec5998393c3d492da06e77d85225ff9e90fb4
dogukanbayrak6767@gmail.com <dogukanbayrak6767@gmail.com>
2022-02-10 17:48:35 +0300

    database added

--- src/main/java/com/patika/paycore/test/controller/CinemaController.java
+++ src/main/java/com/patika/paycore/test/controller/CinemaController.java
@@ -3,11 +3,9 @@ package com.patika.paycore.test.controller;
 import com.patika.paycore.test.model.Cinema;
 import com.patika.paycore.test.service.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,21 +17,33 @@ public class CinemaController {
     @Autowired
     private CinemaService cinemaService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "hello")
+    @GetMapping(path = "/hello")
     public String hello() {
         return " Hello Patika";
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/all")
+    @GetMapping(path = "/all")
     public List<Cinema> getAllCinemas() {
         return cinemaService.getAllCinemas();
+    }
+
+    @PostMapping (path = "/get")
+    public Cinema getCinema(@RequestParam Integer id) {
+        return cinemaService.getCinema(id);
 
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/add")
+    @PostMapping (path = "/add")
     public boolean addCinema(@RequestBody Cinema cinema) {
         return cinemaService.addCinema(cinema);
-
     }
 
+    @PutMapping(value = "/update")
+    public Cinema updateCinema(@RequestBody Cinema cinema){
+        return cinemaService.updateCinema(cinema);
+    }
+    @DeleteMapping(value = "/delete")
+    public boolean deleteCinema(@RequestParam Integer id){
+        return cinemaService.deleteCinema(id);
+    }
 }
